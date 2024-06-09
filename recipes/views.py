5b4b8a8232f865f856d1862d24c6fdfce0294f67
Andre <andre.jp.br@gmail.com>
2024-06-09 15:45:31 -0300

    App recipes.

---
+++ recipes/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    # return HttpResponse("Hello, World Django!")
+    return render(request, "home.html")
+
+
+def contato(request):
+    return HttpResponse("Contato: Django!")
+
+
+def sobre(request):
+    return HttpResponse("Sobre o Django!")
