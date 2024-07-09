70b8108914b67579fa7260cd9ac90a4035eee79f
Andre <andre.jp.br@gmail.com>
2024-07-09 16:12:12 -0300

    Até aula 70. Fim da seção 8.

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 
-from utils.recipes.factory import make_recipe
+# from utils.recipes.factory import make_recipe
 from .models import Recipe
 
 # Create your views here.
@@ -18,13 +18,30 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True
-    ).order_by('-id')
+    # recipes = Recipe.objects.filter(
+    #     category__id=category_id, is_published=True
+    # ).order_by('-id')
+
+    # if not recipes:
+    #     raise Http404('Not found 🥲')
+
+    # recipes = get_list_or_404(
+    #   Recipe, category__id=category_id, is_published=True
+    # )
+
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id, is_published=True
+        ).order_by('-id')
+    )
+    
     return render(
         request,
         'recipes/pages/category.html',
-        context={'recipes': recipes, }
+        context={
+            'recipes': recipes,
+            'title': f"{recipes[0].category.name} - Category | "
+        }
     )
 
 
@@ -33,7 +50,13 @@ def contato(request):
 
 
 def recipe(request, id):
+    # recipe = Recipe.objects.filter(
+    #     pk=id, is_published=True
+    # ).order_by('-id').first()
+
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
