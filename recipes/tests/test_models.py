816fed8956d64006013907f8960ecdffb134b31a
Andre <andre.jp.br@gmail.com>
2024-07-20 14:21:18 -0300

    Seção 9.

---
+++ recipes/tests/test_models.py
@@ -0,0 +1,115 @@
+from django.forms import ValidationError
+from parameterized import parameterized
+
+from .test_recipe_base import Recipe, RecipeTestBase
+
+
+class RecipeModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.recipe = self.make_recipe()
+        return super().setUp()
+
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Test Default Category'),
+            author=self.make_author(username='newuser'),
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
+
+    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
+        self.recipe.title = 'A' * 70
+
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
+
+    def test_recipe_fields_max_length(self):
+        fields = [
+            ('title', 65),
+            ('description', 165),
+            ('preparation_time_unit', 65),
+            ('servings_unit', 65),
+        ]
+
+        for field, max_length in fields:
+            setattr(self.recipe, field, 'A' * (max_length + 1))
+            with self.assertRaises(ValidationError):
+                self.recipe.full_clean()
+
+    def test_recipe_fields_max_length_with_subtest(self):
+        fields = [
+            ('title', 65),
+            ('description', 165),
+            ('preparation_time_unit', 65),
+            ('servings_unit', 65),
+        ]
+
+        for field, max_length in fields:
+            with self.subTest(field=field, max_length=max_length):
+                # só funciona com $ ./manage.py test. Exibe quatro errros
+                # setattr(self.recipe, field, 'A' * (max_length + 0))
+
+                setattr(self.recipe, field, 'A' * (max_length + 1))
+                with self.assertRaises(ValidationError):
+                    self.recipe.full_clean()
+
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length_with_parameterized(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
+
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.preparation_steps_is_html,
+            msg='Recipe preparation_steps_is_html is not False',
+        )
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.is_published,
+            msg='Recipe is_published is not False',
+        )
+
+    def test_recipe_string_representation(self):
+        needed = 'Testing Representation'
+        self.recipe.title = needed
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(
+            str(self.recipe), needed,
+            msg=f'Recipe string representation must be '
+                f'"{needed}" but "{str(self.recipe)}" was received.'
+        )
+
+
+class RecipeCategoryModelTest(RecipeTestBase):
+    def setUp(self) -> None:
+        self.category = self.make_category(
+            name='Category Testing'
+        )
+        return super().setUp()
+
+    def test_recipe_category_model_string_representarion_is_name_field(self):
+        self.assertEqual(str(self.category), self.category.name)
+
+    def test_recipe_category_model_name_max_length_is_65_chars(self):
+        self.category.name = 'A' * 66
+        with self.assertRaises(ValidationError):
+            self.category.full_clean()
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.http import Http404, HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 # from utils.recipes.factory import make_recipe
@@ -34,7 +34,7 @@ def category(request, category_id):
             category__id=category_id, is_published=True
         ).order_by('-id')
     )
-    
+
     return render(
         request,
         'recipes/pages/category.html',
