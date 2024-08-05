782b96356018197c984a818a0b965b881aebb515
Andre <andre.jp.br@gmail.com>
2024-08-05 16:35:50 -0300

    Seção 11.

--- recipes/views.py
+++ recipes/views.py
@@ -1,20 +1,31 @@
+import os
+
 from django.http import Http404, HttpResponse
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.db.models import Q
+from utils.pagination import make_pagination
 
 # from utils.recipes.factory import make_recipe
 from .models import Recipe
 
+
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 # Create your views here.
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     # return HttpResponse("Hello, World Django!")
+
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(
         request,
         "recipes/pages/home.html",
-        context={'recipes': recipes}
+        context={
+            'recipes': page_obj,
+            'pagination_range': pagination_range
+        }
     )
 
 
@@ -36,11 +47,14 @@ def category(request, category_id):
         ).order_by('-id')
     )
 
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(
         request,
         'recipes/pages/category.html',
         context={
-            'recipes': recipes,
+            'recipes': page_obj,
+            'pagination_range': pagination_range,
             'title': f"{recipes[0].category.name} - Category | "
         }
     )
@@ -77,8 +91,12 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     })
